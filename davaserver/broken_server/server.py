216cb88b24524ecad95b982cea70e26f272cd52b
Rares Brezeanu <c_Rares.Brezeanu@viasat.com>
2025-11-26 22:54:24 +0200

    solve server 3 & 4

--- davaserver/broken_server/server.py
+++ davaserver/broken_server/server.py
@@ -4,7 +4,7 @@ from starlette.requests import Request
 from starlette.responses import PlainTextResponse
 
 app = FastAPI()
-students = ["Cosmin", "Edi", "Andrei", "Vlad", "Darius"]
+students = ["Cosmin", "Edi", "Andrei", "Vlad", "Darius", "Rares"]
 
 # Hint: aveti functia checker in middleware cautati pe net ce face ea si ordinea apelarilor
 # La final vreau sa vad ok printeat pe ecran
@@ -13,7 +13,10 @@ def string_checker():
     # Nu schimbi nimic aici decat ce e in if
     my_string = ("In wow you need a lot of alts, alts are characters that you don't play as a main, they are played "
                  "less frequently.")
-    if my_string.split(",")[0].split()[1][::-1] != my_string.split(",")[0].split()[1][::]:
+    # my_string.split(",")[0] => "In wow you need a lot of alts"
+    # .split()[1] => "wow"
+    # [::-1] => "wow" reversed is "wow"
+    if my_string.split(",")[0].split()[1][::-1] == my_string.split(",")[0].split()[1][::]:
         return "ok"
     raise ValueError
 
@@ -30,9 +33,9 @@ def string_checker2():
 
 def db_connect():
     db = mysql.connector.connect(
-        host="localhost",
+        host="mysql_server", #change to container name
         port=3306,
-        user="admins",
+        user="admin",
         passwd="admin",
     )
     return db
@@ -41,7 +44,7 @@ def db_connect():
 @app.middleware("http")
 async def checker(request: Request, call_next):
     try:
-        # string_checker2()
+        string_checker()
         response = await call_next(request)
         return response
     except ValueError as e:
@@ -57,7 +60,11 @@ def home(result=Depends(string_checker)):
     # modifica =="u" trebuie pus sa fie correct
     if result == "ok":
         my_string = "Is best to have druid or mages in order to farm fast because of teleporting and shape-shifting"
-        if my_string.split("fast")[1].split()[-1].split("-")[1][3] == "u":
+        # split on "fast" breaks the string in two parts, index 1 is the second part
+        # split on nothing breaks the string into words, -1 is the last word
+        # split on - breaks shape-shifting into shape and shifting, [1] is shifting
+        # index 3 of shifting is the letter f
+        if my_string.split("fast")[1].split()[-1].split("-")[1][3] == "f":
             return "hello"
         raise ValueError
 
@@ -70,17 +77,24 @@ def data_base(db=Depends(db_connect)):
 
     cursor=db.cursor()
     cursor.execute("USE LAB")
-
+#change varchar to 20 so names fit in
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS STUDENTI (
         id INT AUTO_INCREMENT PRIMARY KEY,
-        name VARCHAR(2) NOT NULL,
+        name VARCHAR(50) NOT NULL,
         pass_not_pass BOOLEAN DEFAULT FALSE
     )
     """)
 
+#typo in the table name fixed
     for name in students:
-        cursor.execute("INSERT INTO STUDENT (name) VALUES (%s)", (name,))
+        cursor.execute("INSERT INTO STUDENTI (name) VALUES (%s)", (name,))
+#update column name only for 1 student
+    cursor.execute("""
+                    update STUDENTI 
+                    set pass_not_pass = TRUE 
+                    where name = %s
+                   """, ("Rares",))
 
     db.commit()
     return "ok"
